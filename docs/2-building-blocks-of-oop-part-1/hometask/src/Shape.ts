0145e55dfadbf0032ba11fcd486f81e5fb8b70a9
Isis Salamanca <isis_salamanca@epam.com>
2022-07-22 11:36:07 -0500

    first exercise

--- docs/2-building-blocks-of-oop-part-1/hometask/src/Shape.ts
+++ docs/2-building-blocks-of-oop-part-1/hometask/src/Shape.ts
@@ -1,3 +1,30 @@
-export abstract class Shape {
-  abstract getType(): string;
+import { Point } from './Point';
+
+export abstract class Shape extends Point {
+    protected color: string
+    protected filled: boolean
+    points: Point[]
+
+    constructor(color = 'green', filled = true, points: Point[]) {
+        super();
+        this.color = color
+        this.filled = filled
+        this.points = points
+    }
+
+    toString(): string {
+        return  `A shape with color of ${this.color} ${this.filled ? 'and filled' : 'not filled'}`
+    }
+
+    getPerimeter(): number {
+        let totalPerimeter: number = 0;
+
+        if (this.points.length) {
+            let perimeter: number[] = [];
+            this.points.map(i => perimeter.push(this.distance(i)))
+            totalPerimeter = perimeter.reduce((partialSum, a) => partialSum + a, 0);
+        }
+
+        return totalPerimeter
+    }
 }
--- docs/2-building-blocks-of-oop-part-1/hometask/src/Triangle.ts
+++ docs/2-building-blocks-of-oop-part-1/hometask/src/Triangle.ts
@@ -1,3 +1,29 @@
 import { Shape } from './Shape';
+import { Point } from './Point';
 
-export class Triangle extends Shape {}
+export class Triangle extends Shape {
+  constructor(p1: Point, p2: Point, p3: Point, color: string, filled: boolean) {
+    super(color, filled, [p1, p2, p3])
+  }
+
+  toString(): string {
+    return `Triangle[v1=(x1, y1),v2=(x2, y2),v3=(x3, y3)]`
+    // The idea here will be use the points instance ? Or stringify with template literal
+    // return `Triangle[v1=(${this.p1.x}, ${this.p1.y}),v2=(${this.p2.x}, ${this.p2.y}),v3=(${this.p3.x}, ${this.p3.y})]`
+  }
+
+  getType(): string {
+    let pointsValues: number[] = [];
+    this.points.map(i => pointsValues.push(this.distance(i)))
+    let type = pointsValues.reduce((cnt, cur) => (cnt[cur] = cnt[cur] + 1 || 1, cnt), {});
+
+    // improve ? switch case ? validate that more than three points is no triangle
+    if (Object.values(type).length === 1) {
+      return 'equilateral triangle'
+    } else if (Object.values(type).length === 2) {
+      return 'isosceles triangle'
+    } else if (Object.values(type).length === 3)  {
+      return 'scalene triangle'
+    } else { return 'this is not a triangle'}
+  }
+}
