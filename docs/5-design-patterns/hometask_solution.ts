00b07072466db11ad4474b75c2242fe1eb017864
Isis Salamanca <isis_salamanca@epam.com>
2022-08-02 23:00:23 -0500

    second-step-design-patterns

--- docs/5-design-patterns/hometask_solution.ts
+++ docs/5-design-patterns/hometask_solution.ts
@@ -6,7 +6,6 @@ class Shipment {
 
   private constructor() {
     this.shipmentID = counter++;
-    console.log("constructor called!");
   }
 
   public static getInstance(): Shipment {
@@ -23,6 +22,47 @@ class Shipment {
 
   public ship(shipId: number, shipFrom: string, shipTo: string, cost: number) {
     return `Shipment ID: ${shipId}. The item was ship from ${shipFrom} and it goes to ${shipTo}. The total cost was ${cost}`
+  }
+}
+
+export interface CalculateCostByShipper {
+  getCost(): number;
+}
+
+const mockedFunction = {
+  getCost() {
+    return 39
+  }
+}
+
+class Shipper {
+  private static instace: Shipper
+  cost: CalculateCostByShipper
+
+  constructor(cost: CalculateCostByShipper) {
+    this.cost = cost
+  }
+
+  public getCost(): void {
+    this.cost.getCost()
+  }
+}
+
+class AirEastShipper implements CalculateCostByShipper {
+  public getCost(): number {
+    return 39
+  }
+}
+
+class ChicagoSprinterShipper implements CalculateCostByShipper {
+  public getCost(): number {
+    return 42
+  }
+}
+
+class PacificParcelShipper implements CalculateCostByShipper {
+  public getCost(): number {
+    return 51
   }
 }
 
@@ -46,5 +86,22 @@ class Client {
 
   ship() {
     this.myInstance.ship(this.shipmentID, this.data.fromAddress, this.data.toAddress, 39)
+  }
+
+  getCost(): number {
+    let result;
+
+    if(this.data.fromZipCode === '' || this.data.fromZipCode === null) {
+      result = new Shipper(new AirEastShipper())
+    }
+
+    if (this.data.fromZipCode.startsWith('1') || this.data.fromZipCode.startsWith('2') || this.data.fromZipCode.startsWith('3')) {
+      result = new Shipper(new AirEastShipper())
+    } else if (this.data.fromZipCode.startsWith('4') || this.data.fromZipCode.startsWith('5') || this.data.fromZipCode.startsWith('6')) {
+      result = new Shipper(new ChicagoSprinterShipper())
+    } else {
+      result = new Shipper(new PacificParcelShipper())
+    }
+    return result;
   }
 }
